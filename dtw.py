0bc10b1469c7669b0fb9207313074fc5a80f4146
chinmay <chinmay0301@gmail.com>
2017-12-28 16:06:20 +0530

    added dynamic time warp function

---
+++ dtw.py
@@ -0,0 +1,51 @@
+from __future__ import print_function
+#import tensorflow as tf
+import numpy as np
+import scipy.fftpack as fft
+from fastdtw import fastdtw
+from scipy.spatial.distance import euclidean
+from scipy import signal 
+
+
+data = np.loadtxt('exoTrain.csv', skiprows=1, delimiter=',')
+np.random.shuffle(data)
+x_train = data[:,1:]
+y_train = data[:,0,np.newaxis] -1 
+y_train = np.concatenate((1-y_train,y_train),axis=1).astype(int)
+data = np.loadtxt('exoTest.csv',skiprows=1,delimiter=',')
+x_test = data[:,1:]
+y_test = data[:, 0, np.newaxis] - 1
+y_test = np.concatenate((1-y_test,y_test),axis=1).astype(int)
+del data
+
+# x_train = ((x_train - np.mean(x_train,axis=1, keepdims = True).reshape(-1,1)) / np.std(x_train,axis=1, keepdims = True).reshape(-1,1))
+# x_test = ((x_test - np.mean(x_test,axis=1, keepdims = True).reshape(-1,1)) / np.std(x_test,axis=1, keepdims = True).reshape(-1,1))
+
+
+
+
+def dtw_add(data):
+	
+	avg=[]
+	std=[]
+	dist_temp = []
+	path_temp = []
+
+	template = np.linspace(1, data.shape[1], 1, endpoint=False)
+
+	for i in range(data.shape[0]):
+		avg.append(np.mean(data[i]))
+		std.append(np.std(data[i])) 
+			
+	
+	for i in range(data.shape[0]):
+		
+		y = -3*std[i]*signal.square(template,duty=0.1)	
+		distance, path = fastdtw(data[i], y, dist=euclidean)
+		dist_temp.append(distance)
+		path_temp.append(path)
+
+	np.save('dtw_values',[dist_temp,avg,std,y])
+
+dtw_add(x_train)
+
