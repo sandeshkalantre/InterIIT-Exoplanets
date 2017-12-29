7708ae10b29fb8546f85cf4b1132fdadae832353
Sheshansh Agrawal <sheshansh@cse.iitb.ac.in>
2017-12-29 14:30:36 +0530

    DTW updated in ConvExoPlanet

--- ConvExoplanet.py
+++ ConvExoplanet.py
@@ -13,53 +13,53 @@ import scipy.signal as sg
 from scipy.spatial.distance import euclidean
 
 def preprocess():
-    # inputting data
-    data = np.loadtxt('exoTrain.csv',skiprows=1,delimiter=',')
+	# inputting data
+	data = np.loadtxt('exoTrain.csv',skiprows=1,delimiter=',')
 
-    np.random.shuffle(data)
+	np.random.shuffle(data)
 
-    x_train = data[:,1:]
-    y_train = data[:, 0, np.newaxis] - 1
-    y_train = np.concatenate((1-y_train,y_train),axis=1).astype(int)
-    dtw_train = [None]*x_train.shape[0]
+	x_train = data[:,1:]
+	y_train = data[:, 0, np.newaxis] - 1
+	y_train = np.concatenate((1-y_train,y_train),axis=1).astype(int)
+	dtw_train = [None]*x_train.shape[0]
 
-    data = np.loadtxt('exoTest.csv',skiprows=1,delimiter=',')
-    x_test = data[:,1:]
-    y_test = data[:, 0, np.newaxis] - 1
-    y_test = np.concatenate((1-y_test,y_test),axis=1).astype(int)
-    dtw_test = [None]*x_test.shape[0]
+	data = np.loadtxt('exoTest.csv',skiprows=1,delimiter=',')
+	x_test = data[:,1:]
+	y_test = data[:, 0, np.newaxis] - 1
+	y_test = np.concatenate((1-y_test,y_test),axis=1).astype(int)
+	dtw_test = [None]*x_test.shape[0]
 
-    del data
-    template = np.linspace(1, x_train.shape[1], 1, endpoint=False)
+	del data
+	template = np.linspace(1, x_train.shape[1], 1, endpoint=False)
 
-    print('Data inputted')
-    print('Preprocessing data')
+	print('Data inputted')
+	print('Preprocessing data')
 
 
-    for  i in range(x_train.shape[0]):
-        if i%100 == 0 :
-            print(i)
-        x_train[i] = sg.medfilt(x_train[i],3)
-        x_train[i] = x_train[i] - sg.medfilt(x_train[i],101)
-        y = -3*np.std(x_train[i])*sg.square(template,duty=0.1)
+	for  i in range(x_train.shape[0]):
+		if i%100 == 0 :
+			print(i)
+		x_train[i] = sg.medfilt(x_train[i],3)
+		x_train[i] = x_train[i] - sg.medfilt(x_train[i],101)
+		y = template*np.mean(x_train[i])
 		y[0] = x_train[i][0]
 		y[-1] = x_train[i][-1]	
-        distance, path = fastdtw(x_train[i], y, dist=euclidean)
-        dtw_train[i] = distance
-    
-    for  i in range(x_test.shape[0]):
-        if i%100 == 0 :
-            print(i)
-        x_test[i] = sg.medfilt(x_test[i],3)
-        x_test[i] = x_test[i] - sg.medfilt(x_test[i],101)
-    	y = -3*np.std(x_test[i])*sg.square(template,duty=0.1)
+		distance, path = fastdtw(x_train[i], y, dist=euclidean)
+		dtw_train[i] = distance
+	
+	for  i in range(x_test.shape[0]):
+		if i%100 == 0 :
+			print(i)
+		x_test[i] = sg.medfilt(x_test[i],3)
+		x_test[i] = x_test[i] - sg.medfilt(x_test[i],101)
+		y = template*np.mean(x_test[i])
 		y[0] = x_test[i][0]
 		y[-1] = x_test[i][-1]	
-        distance, path = fastdtw(x_test[i], y, dist=euclidean)
-        dtw_test[i] = distance
+		distance, path = fastdtw(x_test[i], y, dist=euclidean)
+		dtw_test[i] = distance
 
-    print('Preprocessing done')
-    np.save('preprocessed',[x_train,y_train,dtw_train,x_test,y_test,dtw_test])
+	print('Preprocessing done')
+	np.save('preprocessed',[x_train,y_train,dtw_train,x_test,y_test,dtw_test])
 
 preprocess()
 
@@ -91,6 +91,8 @@ y_train = y_train[500:]
 dtw_train = dtw_train[500:]
 
 
+dtw_test = np.array(dtw_test).reshape([-1,1])
+dtw_train = np.array(dtw_train).reshape([-1,1])
 
 x_train = fft.dct(x_train)
 # x_train[:,:20] = 0
@@ -131,86 +133,86 @@ out3 = 128
 
 # Store layers weight & bias
 weights = {
-    # 1x20 conv, 1 input, 32 outputs
-    'wc1': tf.Variable(tf.random_normal([1, 20, 1, out1])),
-    'wc2': tf.Variable(tf.random_normal([1, 20, out1, out2])),
-    # fully connected,  inputs, 1024 outputs
-    'wd1': tf.Variable(tf.random_normal([((((num_input+1)/2)+1)/2)*out2+1, out3])),
-    # 1024 inputs, 10 outputs (class prediction)
-    'out': tf.Variable(tf.random_normal([out3, num_classes]))
+	# 1x20 conv, 1 input, 32 outputs
+	'wc1': tf.Variable(tf.random_normal([1, 20, 1, out1])),
+	'wc2': tf.Variable(tf.random_normal([1, 20, out1, out2])),
+	# fully connected,  inputs, 1024 outputs
+	'wd1': tf.Variable(tf.random_normal([((((num_input+1)/2)+1)/2)*out2+1, out3])),
+	# 1024 inputs, 10 outputs (class prediction)
+	'out': tf.Variable(tf.random_normal([out3, num_classes]))
 }
 
 biases = {
-    'bc1': tf.Variable(tf.random_normal([out1])),
-    'bc2': tf.Variable(tf.random_normal([out2])),
-    'bd1': tf.Variable(tf.random_normal([out3])),
-    'out': tf.Variable(tf.random_normal([num_classes]))
+	'bc1': tf.Variable(tf.random_normal([out1])),
+	'bc2': tf.Variable(tf.random_normal([out2])),
+	'bd1': tf.Variable(tf.random_normal([out3])),
+	'out': tf.Variable(tf.random_normal([num_classes]))
 }
 
 # Create some wrappers for simplicity
 def conv2d(x, W, b, strides=1):
-    # Conv2D wrapper, with bias and relu activation
-    x = tf.nn.conv2d(x, W, strides=[1, strides, strides, 1], padding='SAME')
-    x = tf.nn.bias_add(x, b)
-    return tf.nn.relu(x)
+	# Conv2D wrapper, with bias and relu activation
+	x = tf.nn.conv2d(x, W, strides=[1, strides, strides, 1], padding='SAME')
+	x = tf.nn.bias_add(x, b)
+	return tf.nn.relu(x)
 
 
 def maxpool2d(x, k=2):
-    # MaxPool2D wrapper
-    return tf.nn.max_pool(x, ksize=[1, 1, k, 1], strides=[1, 1, k, 1],
-                          padding='SAME')
+	# MaxPool2D wrapper
+	return tf.nn.max_pool(x, ksize=[1, 1, k, 1], strides=[1, 1, k, 1],
+						  padding='SAME')
 
 # Create model
 def conv_net(x,DTW,dropout):
-    x = tf.reshape(x, shape=[-1, 1, num_input, 1])
-    print(x.shape)
-    print(tf.shape(x))
-    print("")
-    # Convolution Layer
-    conv1 = conv2d(x, weights['wc1'], biases['bc1'])
-    print(conv1.shape)
-    print(tf.shape(conv1))
-    print("")
-    # Max Pooling (down-sampling)
-    conv1 = maxpool2d(conv1, k=2)
-    print(conv1.shape)
-    print(tf.shape(conv1))
-    print("")
-    conv2 = conv2d(conv1, weights['wc2'], biases['bc2'])
-    print(conv2.shape)
-    print(tf.shape(conv2))
-    print("")
-    # Max Pooling (down-sampling)
-    conv2 = maxpool2d(conv2, k=2)
-    print(conv2.shape)
-    print(tf.shape(conv2))
-    print("")
-    # Fully connected layer
-    # Reshape conv2 output to fit fully connected layer input
-    fc1 = tf.reshape(conv2, [-1, weights['wd1'].get_shape().as_list()[0]-1])
-    print(fc1.shape)
-    print(tf.shape(fc1))
-    print("")
-    fc1 = tf.concat([fc1,DTW],1)
-    print(fc1.shape)
-    print(tf.shape(fc1))
-    print("")
-    fc1 = tf.add(tf.matmul(fc1, weights['wd1']), biases['bd1'])
-    print(fc1.shape)
-    print(tf.shape(fc1))
-    print("")
-    fc1 = tf.nn.relu(fc1)
-    print(fc1.shape)
-    print(tf.shape(fc1))
-    print("")
-     # Apply Dropout
-    fc1 = tf.nn.dropout(fc1, dropout)
-    print(fc1.shape)
-    print(tf.shape(fc1))
-    print("")
-    # Output, class prediction
-    out = tf.add(tf.matmul(fc1, weights['out']), biases['out'])
-    return out
+	x = tf.reshape(x, shape=[-1, 1, num_input, 1])
+	print(x.shape)
+	print(tf.shape(x))
+	print("")
+	# Convolution Layer
+	conv1 = conv2d(x, weights['wc1'], biases['bc1'])
+	print(conv1.shape)
+	print(tf.shape(conv1))
+	print("")
+	# Max Pooling (down-sampling)
+	conv1 = maxpool2d(conv1, k=2)
+	print(conv1.shape)
+	print(tf.shape(conv1))
+	print("")
+	conv2 = conv2d(conv1, weights['wc2'], biases['bc2'])
+	print(conv2.shape)
+	print(tf.shape(conv2))
+	print("")
+	# Max Pooling (down-sampling)
+	conv2 = maxpool2d(conv2, k=2)
+	print(conv2.shape)
+	print(tf.shape(conv2))
+	print("")
+	# Fully connected layer
+	# Reshape conv2 output to fit fully connected layer input
+	fc1 = tf.reshape(conv2, [-1, weights['wd1'].get_shape().as_list()[0]-1])
+	print(fc1.shape)
+	print(tf.shape(fc1))
+	print("")
+	fc1 = tf.concat([fc1,DTW],1)
+	print(fc1.shape)
+	print(tf.shape(fc1))
+	print("")
+	fc1 = tf.add(tf.matmul(fc1, weights['wd1']), biases['bd1'])
+	print(fc1.shape)
+	print(tf.shape(fc1))
+	print("")
+	fc1 = tf.nn.relu(fc1)
+	print(fc1.shape)
+	print(tf.shape(fc1))
+	print("")
+	 # Apply Dropout
+	fc1 = tf.nn.dropout(fc1, dropout)
+	print(fc1.shape)
+	print(tf.shape(fc1))
+	print("")
+	# Output, class prediction
+	out = tf.add(tf.matmul(fc1, weights['out']), biases['out'])
+	return out
 
 # Construct model
 logits = conv_net(X,dtw,keep_prob)
@@ -231,60 +233,61 @@ f1 = 2 * precision * recall / (precision + recall)
 
 # Define loss and optimizer
 loss_op = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(
-    logits=logits, labels=tf.argmax(Y,axis = 1)))
+	logits=logits, labels=tf.argmax(Y,axis = 1)))
 optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
 train_op = optimizer.minimize(loss_op)
 # Initializing the variables
 init = tf.global_variables_initializer()
 
 with tf.Session() as sess:
-    sess.run(init)
+	sess.run(init)
+	# tf.train.Saver()
 
-    # Training cycle
-    for epoch in range(training_epochs):
-        avg_cost = 0.
-        total_batch = int(len(x_train)/batch_size)
-        X_batches = np.array_split(x_train, total_batch)
-        Y_batches = np.array_split(y_train, total_batch)
-        dtw_batches = np.array_split(dtw_train,total_batch)
+	# Training cycle
+	for epoch in range(training_epochs):
+		avg_cost = 0.
+		total_batch = int(len(x_train)/batch_size)
+		X_batches = np.array_split(x_train, total_batch)
+		Y_batches = np.array_split(y_train, total_batch)
+		dtw_batches = np.array_split(dtw_train,total_batch)
 
-        # Loop over all batches
-        for i in range(total_batch):
-            batch_x, batch_y, batch_dtw = X_batches[i], Y_batches[i], dtw_batches[i]
-            # Run optimization op (backprop) and cost op (to get loss value)
-            _, c = sess.run([train_op, loss_op], feed_dict={
-            												X: batch_x
-                                                            ,Y: batch_y
-            												,keep_prob : dropout
-            												,dtw: batch_dtw
-                                                            })
-            # Compute average loss
-            avg_cost += c / total_batch
-        # Display logs per epoch step
-        if epoch % display_step == 0:
-            print("Epoch:", '%04d' % (epoch+1), "cost={:.9f}".format(avg_cost))
-            # if avg_cost < 100.0:
-            print("F1-test:", f1.eval({X: x_test, Y: y_test, keep_prob: 1.0, dtw: dtw_test}))
-        # if avg_cost < cost_thresh:
-        #     break
-    print("Optimization Finished!")
+		# Loop over all batches
+		for i in range(total_batch):
+			batch_x, batch_y, batch_dtw = X_batches[i], Y_batches[i], dtw_batches[i]
+			# Run optimization op (backprop) and cost op (to get loss value)
+			_, c = sess.run([train_op, loss_op], feed_dict={
+															X: batch_x
+															,Y: batch_y
+															,keep_prob : dropout
+															,dtw: batch_dtw
+															})
+			# Compute average loss
+			avg_cost += c / total_batch
+		# Display logs per epoch step
+		if epoch % display_step == 0:
+			print("Epoch:", '%04d' % (epoch+1), "cost={:.9f}".format(avg_cost))
+			# if avg_cost < 100.0:
+			print("F1-test:", f1.eval({X: x_test, Y: y_test, keep_prob: 1.0, dtw: dtw_test}))
+		# if avg_cost < cost_thresh:
+		#     break
+	print("Optimization Finished!")
 
-    save_path = tf.train.Saver().save(sess, "/tmp/model.ckpt")
-    print("Model saved in file: %s" % save_path)
+	save_path = tf.train.Saver().save(sess, "/tmp/model.ckpt")
+	print("Model saved in file: %s" % save_path)
 
-    prediction = pred.eval({X: x_test, Y: y_test, dtw: dtw_test, keep_prob: 1.0})
-    print(np.argmax(prediction,axis = 1))
-    print("TP:", TP.eval({X: x_test, Y: y_test, dtw: dtw_test, keep_prob: 1.0}))
-    print("TN:", TN.eval({X: x_test, Y: y_test, dtw: dtw_test, keep_prob: 1.0}))
-    print("FP:", FP.eval({X: x_test, Y: y_test, dtw: dtw_test, keep_prob: 1.0}))
-    print("FN:", FN.eval({X: x_test, Y: y_test, dtw: dtw_test, keep_prob: 1.0}))
-    print("precision:", precision.eval({X: x_test, Y: y_test, dtw: dtw_test, keep_prob: 1.0}))
-    print("recall:", recall.eval({X: x_test, Y: y_test, dtw: dtw_test, keep_prob: 1.0}))
-    print("F1:", f1.eval({X: x_test, Y: y_test, dtw: dtw_test, keep_prob: 1.0}))
+	prediction = pred.eval({X: x_test, Y: y_test, dtw: dtw_test, keep_prob: 1.0})
+	print(np.argmax(prediction,axis = 1))
+	print("TP:", TP.eval({X: x_test, Y: y_test, dtw: dtw_test, keep_prob: 1.0}))
+	print("TN:", TN.eval({X: x_test, Y: y_test, dtw: dtw_test, keep_prob: 1.0}))
+	print("FP:", FP.eval({X: x_test, Y: y_test, dtw: dtw_test, keep_prob: 1.0}))
+	print("FN:", FN.eval({X: x_test, Y: y_test, dtw: dtw_test, keep_prob: 1.0}))
+	print("precision:", precision.eval({X: x_test, Y: y_test, dtw: dtw_test, keep_prob: 1.0}))
+	print("recall:", recall.eval({X: x_test, Y: y_test, dtw: dtw_test, keep_prob: 1.0}))
+	print("F1:", f1.eval({X: x_test, Y: y_test, dtw: dtw_test, keep_prob: 1.0}))
 
-    # correct_prediction = tf.equal(tf.argmax(pred, 1), tf.argmax(Y, 1))
-    # Calculate accuracy
-    # accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
-    # print("Accuracy:", accuracy.eval({X: x_test, Y: y_test}))
+	# correct_prediction = tf.equal(tf.argmax(pred, 1), tf.argmax(Y, 1))
+	# Calculate accuracy
+	# accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+	# print("Accuracy:", accuracy.eval({X: x_test, Y: y_test}))
 
 
