6c86f586e4df34bcc6432c8d43068f218777a86d
Sheshansh Agrawal <sheshansh@cse.iitb.ac.in>
2018-01-02 01:14:00 +0530

    boost file

--- boost.py
+++ boost.py
@@ -1,17 +1,13 @@
-""" 
-Adapted from
-Project: https://github.com/aymericdamien/TensorFlow-Examples/
-"""
 from __future__ import print_function
 import tensorflow as tf
 import numpy as np
 
+# Load train data here
 [x_train,y_train,dtw_train,x_test,y_test,dtw_test] = np.load('preprocessed_final.npy')
-# [x_train,y_train,dtw_train] = np.load('preprocessed1.npy')
 
-# Parameters
+# Training Parameters
 learning_rate = 0.00003
-training_epochs = 1000
+training_epochs = 000
 batch_size = 300
 display_step = 1
 
@@ -111,25 +107,26 @@ def f1(logits_pred,y):
 	f1 = 2 * precision * recall / (precision + recall)
 	return [f1,[TP,TN,FP,FN]]
 
+# These are the placeholders for logit outputs of individual models
 d1 = tf.placeholder("float", [None, 2])
 d2 = tf.placeholder("float", [None, 2])
-d3 = tf.placeholder("float", [None, 2])
-d4 = tf.placeholder("float", [None, 2])
-# Construct model
-logits_3 = conv_net_2c1d(X,dtw,keep_prob,40,80,500,'1',False)
-logits_4 = conv_net_2c1d(X,dtw,keep_prob,32,16,128,'2',False)
-logits_5 = conv_net_2c1d(X,dtw,keep_prob,30,15,100,'3',False)
-logits_6 = conv_net_2c1d(X,dtw,keep_prob,48,32,128,'4',False)
+namechar1 = '0'
+namechar2 = '1'
+# Construct individual models with the respective model parameters
+logits_1 = conv_net_2c1d(X,dtw,keep_prob,32,16,128,namechar1,False)
+logits_2 = conv_net_2c1d(X,dtw,keep_prob,32,16,128,namechar2,False)
+# In the first run, it evaluates the corresponding outputs of all the models and saves them in ds.npy
+first_run = True
 
-# wd=tf.Variable(tf.random_normal([8,2]))
+# # # If we want single layer neural network
+# wd=tf.Variable(tf.random_normal([4,2]))
 # bd=tf.Variable(tf.random_normal([2]))
+# logits = tf.add(tf.matmul(tf.concat([d1,d2],1), wd),bd)
+
+# # boosting using weighted average case
 w1 = tf.Variable(tf.random_normal([1]),name = 'w1')
 w2 = tf.Variable(tf.random_normal([1]),name = 'w2')
-w3 = tf.Variable(tf.random_normal([1]),name = 'w3')
-w4 = tf.Variable(tf.random_normal([1]),name = 'w4')
-# logits_concat = tf.concat([d1,d2,d3,d4],1)
-# logits = tf.add(tf.matmul(logits_concat, wd),bd)
-logits = (w1*d1+w2*d2+w3*d3+w4*d4)/(w1+w2+w3+w4)
+logits = (w1*d1+w2*d2)/(w1+w2)
 
 # Define loss and optimizer
 loss_op = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(
@@ -141,72 +138,87 @@ train_op = optimizer.minimize(loss_op)
 init = tf.global_variables_initializer()
 with tf.Session() as sess:
 	sess.run(init)
-	tf.train.Saver().restore(sess,"models/model_boost.ckpt")
-	# tf.train.Saver([v for v in tf.global_variables() if v.name[:1]=='1']).restore(sess, "models/model3.ckpt")
-	# tf.train.Saver([v for v in tf.global_variables() if v.name[:1]=='2']).restore(sess, "models/model4.ckpt")
-	# tf.train.Saver([v for v in tf.global_variables() if v.name[:1]=='3']).restore(sess, "models/model5.ckpt")
-	# tf.train.Saver([v for v in tf.global_variables() if v.name[:1]=='4']).restore(sess, "models/model6.ckpt")
-	# Training cycle
+	if first_run==False:
+		tf.train.Saver().restore(sess,"models/model_boost.ckpt")
+	tf.train.Saver([v for v in tf.global_variables() if v.name[:1]==namechar1]).restore(sess, "models/model"+namechar1+".ckpt")
+	tf.train.Saver([v for v in tf.global_variables() if v.name[:1]==namechar2]).restore(sess, "models/model"+namechar2+".ckpt")
 	# writer = tf.summary.FileWriter('./graph', sess.graph);writer.close();
 	total_batch = int(len(x_train)/batch_size)
+	total_batch1 = int(len(x_test)/batch_size)
 	X_batches = np.array_split(x_train, total_batch)
 	Y_batches = np.array_split(y_train, total_batch)
 	dtw_batches = np.array_split(dtw_train,total_batch)
+	X_test_batches = np.array_split(x_test, total_batch1)
+	Y_test_batches = np.array_split(y_test, total_batch1)
+	dtw_test_batches = np.array_split(dtw_test,total_batch1)
 	d1_batches = [None]*total_batch
 	d2_batches = [None]*total_batch
-	d3_batches = [None]*total_batch
-	d4_batches = [None]*total_batch
-	# for i in range(total_batch):
-	# 	print(i)
-	# 	[d1_batches[i],d2_batches[i],d3_batches[i],d4_batches[i]] = sess.run([logits_3,logits_4,logits_5,logits_6],feed_dict={
-	# 			X: X_batches[i]
-	# 			,Y: Y_batches[i]
-	# 			,keep_prob : 1.0
-	# 			,dtw: dtw_batches[i]
-	# 		})
-	# [d1_test,d2_test,d3_test,d4_test] = sess.run([logits_3,logits_4,logits_5,logits_6],feed_dict={
-	# 		X: x_test
-	# 		,Y:y_test
-	# 		,keep_prob : 1.0
-	# 		,dtw: dtw_test
-	# 	})
-	# np.save('ds',[d1_batches,d2_batches,d3_batches,d4_batches,d1_test,d2_test,d3_test,d4_test])
-	[d1_batches,d2_batches,d3_batches,d4_batches,d1_test,d2_test,d3_test,d4_test] = np.load('ds.npy')
+	# Even the test data is divided into batches for support of bigger test sample
+	X_test_batches = np.array_split(x_test, total_batch1)
+	Y_test_batches = np.array_split(y_test, total_batch1)
+	dtw_test_batches = np.array_split(dtw_test,total_batch1)
+	d1_test_batches = [None]*total_batch1
+	d2_test_batches = [None]*total_batch1
+	if first_run==True:
+		for i in range(total_batch):
+			print(i)
+			[d1_batches[i],d2_batches[i]] = sess.run([logits_1,logits_2],feed_dict={
+					X: X_batches[i]
+					,Y: Y_batches[i]
+					,keep_prob : 1.0
+					,dtw: dtw_batches[i]
+				})
+		for i in range(total_batch1):
+			print(i)
+			[d1_test_batches[i],d2_test_batches[i]] = sess.run([logits_1,logits_2],feed_dict={
+					X: X_test_batches[i]
+					,Y: Y_test_batches[i]
+					,keep_prob : 1.0
+					,dtw: dtw_test_batches[i]
+				})
+		np.save('ds',[d1_batches,d2_batches,d1_test_batches,d2_test_batches])
+	# Evaluate the ds's and save for the first time
+	[d1_batches,d2_batches,d1_test_batches,d2_test_batches] = np.load('ds.npy')
+	d1_test_batches = np.array(d1_test_batches)
+	d2_test_batches = np.array(d2_test_batches)
 	for epoch in range(training_epochs):
 		avg_cost = 0.
 		# Loop over all batches
 		for i in range(total_batch):
-			d1_val_batch,d2_val_batch,d3_val_batch,d4_val_batch = d1_batches[i],d2_batches[i],d3_batches[i],d4_batches[i]
+			d1_val_batch,d2_val_batch = d1_batches[i],d2_batches[i]
 			# Run optimization op (backprop) and cost op (to get loss value)
 			_, c = sess.run([train_op, loss_op], feed_dict={
 															d1: d1_batches[i],
 															d2: d2_batches[i],
-															d3: d3_batches[i],
-															d4: d4_batches[i],
 															Y: Y_batches[i]
 														})
-			# logits_test = logits.eval({
-			# 							d1: d1_batches[i],
-			# 							d2: d2_batches[i],
-			# 							d3: d3_batches[i],
-			# 							d4: d4_batches[i],
-			# 							Y: Y_batches[i]
-			# 						})
-			# print("F1-test:", f1(logits_test,Y_batches[i]))
+			logits_test = logits.eval({
+										d1: d1_batches[i],
+										d2: d2_batches[i],
+										Y: Y_batches[i]
+									})
+			print("F1-test:", f1(logits_test,Y_batches[i]))
 			# Compute average loss
 			avg_cost += c / total_batch
 		# Display logs per epoch step
 		if epoch % display_step == 0:
 			print("Epoch:", '%04d' % (epoch+1), "cost={:.9f}".format(avg_cost))
-			logits_test = logits.eval({
-								d1: d1_test,
-								d2: d2_test,
-								d3: d3_test,
-								d4: d4_test,
-								Y: y_test
-							})
-			print("logits")
-			print("F1-test:", f1(logits_test,y_test))
+			for i in range(total_batch1):
+				logits_test = logits.eval({
+									d1: d1_test_batches[i],
+									d2: d2_test_batches[i],
+									Y: Y_test_batches[i]
+								})
+				# print(logits_test)
+				print("F1-test:", f1(logits_test,Y_test_batches[i]))
 	print("Optimization Finished!")
+	for i in range(total_batch1):
+		logits_test = logits.eval({
+							d1: d1_test_batches[i],
+							d2: d2_test_batches[i],
+							Y: Y_test_batches[i]
+						})
+		# print(logits_test)
+		print("F1-test:", f1(logits_test,Y_test_batches[i]))
 	save_path = tf.train.Saver().save(sess, "models/model_boost.ckpt")
 	print("Model saved in file: %s" % save_path)
